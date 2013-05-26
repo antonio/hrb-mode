2369d237d0f285a2e78da0bc9ca97d4fd9fa3c9f
Christian Kruse <cjk@wwwtech.de>
2013-05-26 02:03:52 +0200

    added GPL license and copyright information

--- hrb-mode.el
+++ hrb-mode.el
@@ -1,5 +1,28 @@
 ;; -*- coding: utf-8 -*-
 
+;;; hrb-block.el - highlight ruby blocks
+
+;; Copyright (C) 2013 Christian Kruse
+
+;; Author: Christian Kruse <cjk@defunct.ch>
+;; Version: 0.0.2
+;; Keywords: languages, faces, ruby
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.	If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
 (require 'ruby-mode)
 
 (defgroup hrb nil
@@ -133,4 +156,5 @@
   )
 
 (provide 'hrb-mode)
+
 ;; eof
